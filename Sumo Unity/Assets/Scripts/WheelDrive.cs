c805f55f6a6d9504104b7a6380e51981c1074811
Reeshad Khan <65279631+Reeshad-Khan@users.noreply.github.com>
2021-04-01 17:08:29 +0600

    updated

--- Sumo Unity/Assets/Scripts/WheelDrive.cs
+++ Sumo Unity/Assets/Scripts/WheelDrive.cs
@@ -30,21 +30,17 @@ public class WheelDrive : MonoBehaviour
 	[Tooltip("The vehicle's drive type: rear-wheels drive, front-wheels drive or all-wheels drive.")]
 	public DriveType driveType;
 
-	private bool isReverse = false;
     private WheelCollider[] m_Wheels;
-    private float speed;
 
     // Find all the WheelColliders down in the hierarchy.
 	void Start()
 	{
-		//initialising the Logitech wheel module
-		Debug.Log("SteeringInit:" + LogitechGSDK.LogiSteeringInitialize(false));
-		
 		var inert = GetComponent<Rigidbody>().inertiaTensor;
 	
-		
+		print(maxTorque);
+		print(inert);
 		m_Wheels = GetComponentsInChildren<WheelCollider>();
-		
+		print(maxTorque/inert.x);
 		
 
 		for (int i = 0; i < m_Wheels.Length; ++i) 
@@ -59,77 +55,28 @@ public class WheelDrive : MonoBehaviour
 			}
 		}
 	}
-	
-
 
 	
-	void OnApplicationQuit()
-	{
-		Debug.Log("SteeringShutdown:" + LogitechGSDK.LogiSteeringShutdown());
-	}
-     //Displays the Speed that the ego-vehicle is currently travelling at
-	private void OnGUI()
-	{
-		int mstokmh = (int)( speed * 3.6);
-		
-		GUI.Box (new Rect (Screen.width - 100,Screen.height - 50,100,50), mstokmh.ToString() + " km/h");
-	}
 
 	// This is a really simple approach to updating wheels.
 	// We simulate a rear wheel drive car and assume that the car is perfectly symmetric at local zero.
 	// This helps us to figure our which wheels are front ones and which are rear.
 	void Update()
 	{
-		if (LogitechGSDK.LogiUpdate() && LogitechGSDK.LogiIsConnected(0))
-		{
-			LogitechGSDK.LogiPlaySpringForce(0, 0, 50, 100);
-		}
-		speed = GetComponent<Rigidbody>().velocity.magnitude;
+		float speed = GetComponent<Rigidbody>().velocity.magnitude;
 		
 		m_Wheels[0].ConfigureVehicleSubsteps(criticalSpeed, stepsBelow, stepsAbove);
-			float angle = 0;
-		
 
-		if (Input.GetKey(KeyCode.JoystickButton12) && isReverse == false)
-		{
-			
-			isReverse = true;
-			
-		}
-		if (Input.GetKey(KeyCode.JoystickButton13) && isReverse)
-		{
-			
-			isReverse = false;
-			
-		}
+		float angle = maxAngle * Input.GetAxis("Horizontal");
 		
-		if (Input.GetAxis("Horizontal" )> 0.1175f || Input.GetAxis("Horizontal" )< -0.1175f)
-		{
-			
-			 angle = maxAngle * Input.GetAxis("Horizontal");
-		}
-
-		float torque = 0;
-		if (Input.GetAxis("Vertical") > 0 && isReverse == false){
-			 torque = maxTorque * Input.GetAxis("Vertical");
-		}
-		if (Input.GetAxis("Vertical")> 0 && isReverse == true){
-			torque = maxTorque * Input.GetAxis("Vertical")*-1;
-		}
-		if (speed >= 13.8889f)
+		float torque = maxTorque * Input.GetAxis("Vertical");
+		if (speed >= 16.67)
 		{
 			torque = 0;
 		}
-
-		
-		float handBrake = 0;
-		if (Input.GetAxis("Brake") > 0)
-		{
-			handBrake = brakeTorque;
-		}
 		
 
-		
+		float handBrake = Input.GetKey(KeyCode.X) ? brakeTorque : 0;
 
 		foreach (WheelCollider wheel in m_Wheels)
 		{
